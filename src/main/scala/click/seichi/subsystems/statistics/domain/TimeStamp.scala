0808e48c9d5827c24c23ee16547541f35b47702e
rito528 <rotoyutoriapp@gmail.com>
2022-09-12 20:21:08 +0900

    feat: Defining a function that generates a TimeStamp from now time

--- src/main/scala/click/seichi/subsystems/statistics/domain/TimeStamp.scala
+++ src/main/scala/click/seichi/subsystems/statistics/domain/TimeStamp.scala
@@ -8,11 +8,20 @@ case class TimeStamp(value: LocalDateTime)
 object TimeStamp {
 
   /**
-   * @return yyyy/MM/dd HH:mm:ssの形式でフォーマットされた文字列から[[TimeStamp]]型を生成して返す作用
+   * @return yyyy/MM/dd HH:mm:ssの形式でフォーマットされた文字列から[[TimeStamp]]型を生成して返す
    */
   def fromString(stringDateTime: String): TimeStamp = {
     val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
     val dateTime = LocalDateTime.parse(stringDateTime, dateTimeFormatter)
+
+    TimeStamp(dateTime)
+  }
+
+  /**
+   * @return 現在の時刻から[[TimeStamp]]を生成して返す作用
+   */
+  def fromNow: TimeStamp = {
+    val dateTime = LocalDateTime.now()
 
     TimeStamp(dateTime)
   }
