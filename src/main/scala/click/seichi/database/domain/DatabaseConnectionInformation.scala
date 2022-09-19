8e0e27ead7f6bac22757ca85d80303d99413eb58
rito528 <rotoyutoriapp@gmail.com>
2022-09-19 15:49:25 +0900

    fix: Added userinformation to DatabaseConnetionInformation.

--- src/main/scala/click/seichi/database/domain/DatabaseConnectionInformation.scala
+++ src/main/scala/click/seichi/database/domain/DatabaseConnectionInformation.scala
@@ -1,7 +1,13 @@
 package click.seichi.database.domain
 
-case class DatabaseConnectionInformation(host: String, password: String, port: Int) {
+case class DatabaseConnectionInformation(
+  host: String,
+  user: String,
+  password: String,
+  port: Int
+) {
   require(host != null, "ホスト名が不正です。")
+  require(user != null, "ユーザー名が不正です。")
   require(password != null, "パスワードが不正です。")
   // ポートの範囲についての参考ページ: https://www.ibm.com/docs/ja/i/7.3?topic=ssw_ibm_i_73/cl/addtcpport.htm
   require(1 <= port && port <= 65535, "ポートは1以上65535以下で指定してください。")
