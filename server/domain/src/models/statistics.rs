0b6b149e5d2b86de2b6c47e968ff32e9cd8db143
Ryosuke <korygm33@gmail.com>
2023-05-25 17:06:50 +0100

    chore: 統計量に Eq を derive する

--- server/domain/src/models/statistics.rs
+++ server/domain/src/models/statistics.rs
@@ -1,13 +1,13 @@
 use std::fmt::Debug;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BreakCount(pub u64);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BuildCount(pub u64);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PlayTicks(pub u64);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct VoteCount(pub u64);
