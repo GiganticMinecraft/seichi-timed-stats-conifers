0e1e087dc4da77f345d4d593b365ca60cb248fd9
Kohei Ota (inductor) <kela@inductor.me>
2023-06-12 02:34:50 +0900

    Update server/ingestor/src/config.rs
    Co-authored-by: Kory <6561358+kory33@users.noreply.github.com>

--- server/ingestor/src/config.rs
+++ server/ingestor/src/config.rs
@@ -2,7 +2,7 @@ use once_cell::sync::Lazy;
 
 #[derive(serde::Deserialize, Debug)]
 pub struct Env {
-    pub name: String,
+    pub environment_name: String,
 }
 
 pub static ENV: Lazy<Env> = Lazy::new(|| envy::prefixed("ENV_").from_env::<Env>().unwrap());
